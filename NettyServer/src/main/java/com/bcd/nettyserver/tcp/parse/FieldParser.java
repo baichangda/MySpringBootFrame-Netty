d2e706897383b5b98810de4d4268e74861380053
baichangda <471267877@qq.com>
2020-03-26 01:44:54 +0800

    c

--- NettyServer/src/main/java/com/bcd/nettyserver/tcp/parse/FieldParser.java
+++ NettyServer/src/main/java/com/bcd/nettyserver/tcp/parse/FieldParser.java
@@ -5,7 +5,23 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 
 public interface FieldParser<T> {
+    /**
+     * 从data中取出len字节解析为对应的结果
+     * @param data 数据集
+     * @param len 数据长度
+     * @param ext 附加信息
+     * @return
+     */
     T parse(ByteBuf data,int len, Object... ext);
+
+    /**
+     * 把对象转换为hex字符串
+     * @param data 对象
+     * @param len 转换后字节长度
+     * @param ext 附加信息
+     * @return
+     */
+    String toHex(T data,int len,Object ... ext);
 
     /**
      * 按照期望长度和实际长度取出数据
@@ -22,12 +38,16 @@ public interface FieldParser<T> {
             data.skipBytes(actualByteLen-expectByteLen);
             return data.readBytes(actualByteLen);
         }else {
-            ByteBuf temp= Unpooled.buffer();
+            ByteBuf temp= Unpooled.buffer(actualByteLen);
             for (int i = 1; i <= expectByteLen - actualByteLen; i++) {
                 temp.writeByte(0);
             }
             temp.writeBytes(data, actualByteLen);
             return temp;
         }
+    }
+
+    default String toHex(){
+        
     }
 }
