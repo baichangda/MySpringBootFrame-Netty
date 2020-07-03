8eb05736d43dacd7bcae53ea608f4c898bbc766b
baichangda <471267877@qq.com>
2020-07-03 23:33:46 +0800

    c

--- NettyServer/src/main/java/com/bcd/nettyserver/http/result/handler/NettyHttpResultHandler.java
+++
@@ -1,9 +0,0 @@
-package com.bcd.nettyserver.http.result.handler;
-
-
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.HttpRequest;
-
-public interface NettyHttpResultHandler {
-    void handleResult(Object res, HttpRequest httpRequest, ChannelHandlerContext context);
-}
